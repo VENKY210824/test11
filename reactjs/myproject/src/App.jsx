06ea9111fbd4ed51290532e7d57a6f94c02cdc3c
venky210824 <venkygollapelly4@gmail.com>
2025-02-18 10:24:26 +0530

    git for ready to push the files

---
+++ reactjs/myproject/src/App.jsx
@@ -0,0 +1,12 @@
+import Test from './components/Test'
+import Test1 from './components/Test1'
+function App(){
+  return(
+    <div>
+      <h1>This is App.jsx  code</h1>
+      <Test/>
+      <Test1/>
+    </div>
+  )
+}
+export default App;
